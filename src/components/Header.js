ff0da165667a1d60fd324b68d716af51e214adf9
unknown <18103052@iubat.edu>
2020-03-13 11:13:18 +0600

    complete react first milestone

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../images/logo.png';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <img src={logo} alt=""/>
+            <nav>
+                <a href="/Shop">Shop</a>
+                <a href="/review">Order review</a>
+                <a href="/manage">Manage Inventory here</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
